89055bd28cc76e3371616de76d40102a2ff91d70
anderson <anderson.oliva@gmail.com>
2019-02-19 23:38:38 -0300

    Inclusao do Screenshot e Data easytest

--- src/test/java/tests/informacoesUsuarioTest.java
+++ src/test/java/tests/informacoesUsuarioTest.java
@@ -1,23 +1,34 @@
 package tests;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Parallel;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
+import org.junit.*;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.ScreenshotException;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import suporte.Generator;
+import suporte.Screenshot;
 
 import javax.print.attribute.standard.MediaSize;
 import java.util.concurrent.TimeUnit;
 
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(filePaths = "informacoesUsuarioTestData.csv")
 public class informacoesUsuarioTest {
     private WebDriver navegador;
+
+    @Rule
+    public TestName test = new TestName();
 
     @Before
     public void setup(){
@@ -38,32 +49,35 @@ public class informacoesUsuarioTest {
     }
 
     @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(@Param(name="tipo")String tipo, @Param(name="contato")String contato, @Param(name="mensagem")String mensagemEsperada){
         navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
         WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
 
         WebElement campoType = popupAddMoreData.findElement(By.name("type"));
-        new Select(campoType).selectByVisibleText("Phone");
+        new Select(campoType).selectByVisibleText(tipo);
 
-        popupAddMoreData.findElement(By.name("contact")).sendKeys("+5511984131888");
-
+        popupAddMoreData.findElement(By.name("contact")).sendKeys(contato);
         popupAddMoreData.findElement(By.linkText("SAVE")).click();
 
         WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
         String mensagem = mensagemPop.getText();
-        Assert.assertEquals("Your contact has been added!", mensagem);
+        Assert.assertEquals(mensagemEsperada, mensagem);
     }
 
-    @Test
+    //@Test
     public void removerUmContatoDeUsuario(){
         //Clicar no elemento pelo xpath (identificar pelo text e excluir o proximo elemento "a")
-        navegador.findElement(By.xpath("//span[text()=\"+5511 33334444\"]/following-sibling::a")).click();
+        navegador.findElement(By.xpath("//span[text()=\"+5511 984131313\"]/following-sibling::a")).click();
         //Confirmar janela do java script
         navegador.switchTo().alert().accept();
         //Validar que a mensagem apresentada foi Rest in peace, dear phone!
         WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
         String mensagem = mensagemPop.getText();
         Assert.assertEquals("Rest in peace, dear phone!", mensagem);
+
+        String screenshotArquivo = "C:\\Users\\OLIVA\\Desktop\\test-report\\taskit\\" + Generator.dataHoraParaArquivo() + test.getMethodName() + ".png";
+        Screenshot.tirar(navegador, screenshotArquivo);
+
         //Aguardar até 10 segundos para que a janela desapareça
         WebDriverWait aguardar = new WebDriverWait(navegador, 10);
         aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
